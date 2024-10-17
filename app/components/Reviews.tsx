c8a5bacbe15e162ff5e424808890130a8c074aa5
wilder09 <aadityapanchal289@gmail.com>
2024-10-17 00:50:34 -0500

    added names

--- app/components/Reviews.tsx
+++ app/components/Reviews.tsx
@@ -1,91 +1,158 @@
-import React from 'react'
+import React from "react";
 
 function Reviews() {
   return (
     <section className="bg-white">
-  <div className="mx-auto max-w-screen-xl px-4 py-12 sm:px-6 lg:px-8 lg:py-16">
-    <h2 className="text-center text-4xl font-bold tracking-tight text-gray-900 sm:text-5xl">
-      Testimonials!
-    </h2>
+      <div className="mx-auto max-w-screen-xl px-4 py-12 sm:px-6 lg:px-8 lg:py-16">
+        <h2 className="text-center text-4xl font-bold tracking-tight text-gray-900 sm:text-5xl">
+          Testimonials!
+        </h2>
 
-    <div className="mt-8 [column-fill:_balance] sm:columns-2 sm:gap-6 lg:columns-3 lg:gap-8">
-      <div className="mb-8 sm:break-inside-avoid">
-        <blockquote className="rounded-lg bg-gray-50 p-6 shadow-sm sm:p-8">
+        <div className="mt-8 [column-fill:_balance] sm:columns-2 sm:gap-6 lg:columns-3 lg:gap-8">
+          <div className="mb-8 sm:break-inside-avoid">
+            <blockquote className="rounded-lg bg-gray-50 p-6 shadow-sm sm:p-8">
+              <div>
+                <div className="flex justify-center gap-0.5 text-green-500">
+                  <p className="mt-0.5 text-lg font-medium text-gray-900">
+                    Paul Starr
+                  </p>
+                </div>
+              </div>
 
-          <p className="mt-4 text-blue-950">
-          My journey with RMA has been incredible so far. From day one, I’ve been working closely with my mentor, and I’ve learned so much about bioinformatics. The freedom to explore and the guidance I’ve received have really helped me grow as a researcher. I couldn’t have asked for a better experience, and I’m excited to see where this project leads.
-          </p>
-        </blockquote>
+              <p className="mt-4 text-blue-950">
+                My journey with RMA has been incredible so far. From day one,
+                I’ve been working closely with my mentor, and I’ve learned so
+                much about bioinformatics. The freedom to explore and the
+                guidance I’ve received have really helped me grow as a
+                researcher. I couldn’t have asked for a better experience, and
+                I’m excited to see where this project leads.
+              </p>
+            </blockquote>
+          </div>
+
+          <div className="mb-8 sm:break-inside-avoid">
+            <blockquote className="rounded-lg bg-gray-50 p-6 shadow-sm sm:p-8">
+            <div>
+                <div className="flex justify-center gap-0.5 text-green-500">
+                  <p className="mt-0.5 text-lg font-medium text-gray-900">
+                    Kathleen Daniels
+                  </p>
+                </div>
+              </div>
+              <p className="mt-4 text-blue-950">
+                RMA has truly exceeded my expectations! Working with my mentor
+                on our project about sustainable energy has been an eye-opening
+                experience. We’ve already gathered some fascinating data, and I
+                can’t wait to see how it all comes together. This program has
+                given me the chance to connect with an expert in the field and
+                explore econometrics, a topic I’m passionate about. I’m grateful
+                for the opportunity to be part of this mentorship.
+              </p>
+            </blockquote>
+          </div>
+
+          <div className="mb-8 sm:break-inside-avoid">
+            <blockquote className="rounded-lg bg-gray-50 p-6 shadow-sm sm:p-8">
+            <div>
+                <div className="flex justify-center gap-0.5 text-green-500">
+                  <p className="mt-0.5 text-lg font-medium text-gray-900">
+                    Michael Trencher
+                  </p>
+                </div>
+              </div>
+              <p className="mt-4 text-blue-950">
+                When I signed up for RMA, I expected it to be a scam program
+                because it sounded too good to be true. Now, I am thankful I
+                made the decision to sign up because I never would have met
+                Lucas Eddy, a graduate student who has guided my research. I
+                would recommend everyone to apply because my experience has been
+                positive.
+              </p>
+            </blockquote>
+          </div>
+
+          <div className="mb-8 sm:break-inside-avoid">
+            <blockquote className="rounded-lg bg-gray-50 p-6 shadow-sm sm:p-8">
+            <div>
+                <div className="flex justify-center gap-0.5 text-green-500">
+                  <p className="mt-0.5 text-lg font-medium text-gray-900">
+                    Adhihan Radakrishnan
+                  </p>
+                </div>
+              </div>
+              <p className="mt-4 text-blue-950">
+                The RMA experience has been unique and different than other
+                mentorship programs I have been involved in. They give way more
+                independence by letting us work directly with the mentor with no
+                intervention from them. I am thankful to RMA for this unique
+                experience!
+              </p>
+            </blockquote>
+          </div>
+
+          <div className="mb-8 sm:break-inside-avoid">
+            <blockquote className="rounded-lg bg-gray-50 p-6 shadow-sm sm:p-8">
+            <div>
+                <div className="flex justify-center gap-0.5 text-green-500">
+                  <p className="mt-0.5 text-lg font-medium text-gray-900">
+                    Elimilian Lipnevich
+                  </p>
+                </div>
+              </div>
+              <p className="mt-4 text-blue-950">
+                My experience has been pretty good with RMA! We made contact
+                with the professor and other members of our group and got to
+                work immediately, finding topics, journals, reference articles,
+                etc. which is a good direction to go in for sure! This has been
+                a pretty cool program to find and join for sure and I’m happy
+                about the opportunity, especially in a topic that I am very
+                interested in. I am very glad to be paired with my professor and
+                respect her and her work so I’m excited to see where it goes in
+                the future!
+              </p>
+            </blockquote>
+          </div>
+
+          <div className="mb-8 sm:break-inside-avoid">
+            <blockquote className="rounded-lg bg-gray-50 p-6 shadow-sm sm:p-8">
+            <div>
+                <div className="flex justify-center gap-0.5 text-green-500">
+                  <p className="mt-0.5 text-lg font-medium text-gray-900">
+                    Alex Knight
+                  </p>
+                </div>
+              </div>
+              <p className="mt-4 text-blue-950">
+                I was skeptical about how much I could learn from a virtual
+                mentorship, but RMA has proved me wrong. My mentor has been
+                extremely helpful, and we’ve already started drafting our
+                research paper on climate policy. I love being able to work with
+                my mentor and a group on our own projects. I’m really thankful
+                for this experience!
+              </p>
+            </blockquote>
+          </div>
+          <div className="mb-8 sm:break-inside-avoid">
+            <blockquote className="rounded-lg bg-gray-50 p-6 shadow-sm sm:p-8">
+            <div>
+                <div className="flex justify-center gap-0.5 text-green-500">
+                  <p className="mt-0.5 text-lg font-medium text-gray-900">
+                    Vanshika Burman
+                  </p>
+                </div>
+              </div>
+              <p className="mt-4 text-blue-950">
+                I would like to thank RMA for this opportunity. So far, research
+                with Professor Giasseti has been going very well. We are about
+                to begin writing our review based on topics like gene therapy
+                and AI.{" "}
+              </p>
+            </blockquote>
+          </div>
+        </div>
       </div>
-
-      <div className="mb-8 sm:break-inside-avoid">
-        <blockquote className="rounded-lg bg-gray-50 p-6 shadow-sm sm:p-8">
-          
-
-          <p className="mt-4 text-blue-950">
-          RMA has truly exceeded my expectations! Working with my mentor on our project about sustainable energy has been an eye-opening experience. We’ve already gathered some fascinating data, and I can’t wait to see how it all comes together. This program has given me the chance to connect with an expert in the field and explore econometrics, a topic I’m passionate about. I’m grateful for the opportunity to be part of this mentorship.
-          </p>
-        </blockquote>
-      </div>
-
-      <div className="mb-8 sm:break-inside-avoid">
-        <blockquote className="rounded-lg bg-gray-50 p-6 shadow-sm sm:p-8">
-          
-
-          <p className="mt-4 text-blue-950">
-          When I signed up for RMA, I expected it to be a scam program because it sounded too good to be true. Now, I am thankful I made the decision to sign up because I never would have met Lucas Eddy, a graduate student who has guided my research. I would recommend everyone to apply because my experience has been positive. 
-          </p>
-        </blockquote>
-      </div>
-
-      <div className="mb-8 sm:break-inside-avoid">
-        <blockquote className="rounded-lg bg-gray-50 p-6 shadow-sm sm:p-8">
-          
-
-          <p className="mt-4 text-blue-950">
-          The RMA experience has been unique and different than other mentorship programs I have been involved in. They give way more independence by letting us work directly with the mentor with no intervention from them. I am thankful to RMA for this unique experience!
-          </p>
-        </blockquote>
-      </div>
-
-      <div className="mb-8 sm:break-inside-avoid">
-        <blockquote className="rounded-lg bg-gray-50 p-6 shadow-sm sm:p-8">
-          
-
-          <p className="mt-4 text-blue-950">
-          My experience has been pretty good with RMA! We made contact with the professor and other members of our group and got to work immediately, finding topics, journals, reference articles, etc. which is a good direction to go in for sure! This has been a pretty cool program to find and join for sure and I’m happy about the opportunity, especially in a topic that I am very interested in. I am very glad to be paired with my professor and respect her and her work so I’m excited to see where it goes in the future!
-
-          </p>
-        </blockquote>
-      </div>
-
-      
-
-      <div className="mb-8 sm:break-inside-avoid">
-        <blockquote className="rounded-lg bg-gray-50 p-6 shadow-sm sm:p-8">
-          
-
-          <p className="mt-4 text-blue-950">
-          I was skeptical about how much I could learn from a virtual mentorship, but RMA has proved me wrong. My mentor has been extremely helpful, and we’ve already started drafting our research paper on climate policy. I love being able to work with my mentor and a group on our own projects. I’m really thankful for this experience!
-          </p>
-        </blockquote>
-      </div>
-      <div className="mb-8 sm:break-inside-avoid">
-        <blockquote className="rounded-lg bg-gray-50 p-6 shadow-sm sm:p-8">
-          
-
-          <p className="mt-4 text-blue-950">
-          I would like to thank RMA for this opportunity. So far, research with Professor Giasseti has been going very well. We are about to begin writing our review based on topics like gene therapy and AI.          </p>
-        </blockquote>
-      </div>
-
-      
-
-     
-    </div>
-  </div>
-</section>
-  )
+    </section>
+  );
 }
 
-export default Reviews
+export default Reviews;
