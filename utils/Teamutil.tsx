d469cbd49fce5952ed4103191f66b4cb7e808b7e
IshaanHemrajani <31942445+IshaanHemrajani@users.noreply.github.com>
2025-06-01 12:57:58 +0530

    Update Teamutil.tsx

--- utils/Teamutil.tsx
+++ utils/Teamutil.tsx
@@ -6,44 +6,14 @@ export interface types {
 
 export const Teamutil: types[] = [
   {
-    image: '/Ishaan.jpeg',
-    name: 'Ishaan Hemrajani',
-    desc: 'Co-Founder/CEO',
-  },
-  {
-    image: '/Fahim.jpeg',
-    name: 'Fahim Jahangir',
-    desc: 'Co-Founder',
-  },
-  {
-    image: '/Niam.jpeg',
-    name: 'Niam Jasani',
-    desc: 'Co-Founder',
-  },
-  {
-    image: '/image3.jpeg',
-    name: 'Sanjay Sreedhar',
-    desc: 'Co-Founder',
-  },
-  {
-    image: '/Mihir.jpeg',
-    name: 'Mihir Gannavarapu',
-    desc: 'Co-Founder',
-  },
-  {
-    image: '',
-    name: 'Sudhanva Vasista',
-    desc: 'Chief of Operations',
-  },
-  {
     image: '/aadi.jpeg',
     name: 'Aaditya Panchal',
     desc: 'Program Director',
   },
   {
     image: '',
-    name: 'Rishan Hemrajani',
-    desc: 'Program Director',
+    name: 'Rahul Pai',
+    desc: 'Social Media Manager',
   },
 ];
 
